74c68f59de4262b98ce867b573040c9dc78a6ca8
Mohammadjavad Raadi <mohammadjavad.raadi@sentia.com>
2022-07-20 14:25:09 +0200

    Setting things up

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,22 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Mohammadjavad Raadi',
-  authorAddress: 'mohammadjavad.raadi@sentia.com',
+  author: 'Sentia MPC',
+  authorAddress: 'support.mpc@sentia.com',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'halloumi-cross-account-parameter-store',
-  repositoryUrl: 'https://github.com/mohammadjavad.raadi/halloumi-cross-account-parameter-store.git',
+  repositoryUrl:
+    'https://github.com/sentialabs/halloumi-cross-account-parameter-store.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  deps: ['aws-sdk'],
+  description:
+    'A custom CDK construct to manage a parameter across an AWS account. This construct creates a Lambda-backed custom resource using AWS CloudFormation that handles assuming a role on the target AWS account and puts, updates or deletes a parameter on that account. Role and parameter related variables are passed to the construct and are used by the function to perform these operations.',
+  devDeps: ['@types/cfn-response'],
+  bundledDeps: ['aws-sdk'],
+  publishToPypi: {
+    distName: 'halloumi-cross-account-parameter-store',
+    module: 'halloumi_cross_account_parameter_store',
+  },
+  antitamper: false,
 });
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="halloumi-cross-account-parameter-store.Hello"></a>
+
+#### Initializers <a name="Initializers" id="halloumi-cross-account-parameter-store.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'halloumi-cross-account-parameter-store'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#halloumi-cross-account-parameter-store.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="halloumi-cross-account-parameter-store.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
